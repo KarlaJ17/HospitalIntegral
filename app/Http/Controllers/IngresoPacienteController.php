ba4fb1cd8ec530e2b21ef3adb185ca550547943b
Shers <shersportillo@gmail.com>
2024-04-04 10:26:37 -0600

    renombrado

--- app/Http/Controllers/IngresoPacienteController.php
+++ app/Http/Controllers/IngresoPacienteController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Models\IngresoPaciente; //Llamar al modelo
 
-class IngresoPaciente extends Controller
-{
+class IngresoPacienteController extends Controller{
+
+
     public function index(){
         // SELECT * FROM paciente_id;
         $pacientes = IngresoPacientes::all(); //Tal cual cómo lo indica el módelo
@@ -16,14 +17,14 @@ class IngresoPaciente extends Controller
         if (count($pacientes)<1){
             return response()->json(array(
                 'message'=> "No se encontró perfiles de pacientes registrados.",
-                'data'=> $paciente, //Extraer información
+                'data'=> $perfil_pacientes, //Extraer información
                 'code'=> 404,
             ),404);
         }
         
         return response()->json(array(
             'message'=> "Perfiles de pacientes registrados",
-            'data'=> $doctores,
+            'data'=> $perfil_pacientes,
             'code'=> 200, 
         ),200);
     
