079f8c5530a1e07b8be692ed2c4ae0b02fe7166e
Shers <shersportillo@gmail.com>
2024-04-04 09:02:05 -0600

    edición de tablas de pacientes

--- app/Http/Controllers/PerfilPacienteController.php
+++ app/Http/Controllers/PerfilPacienteController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\PerfilPaciente; //llamar al modelo
+
 class PerfilPacienteController extends Controller
 {
-    //
+    public function index(){
+        //SELECT * FROM perfil
+    }
 }
