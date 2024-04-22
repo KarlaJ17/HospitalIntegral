39657a0c0d1bc94a0eb2d7ff7d7db7bca191b0e6
KarlaJ17 <karlajotorres17@gmail.com>
2024-04-22 14:57:48 -0600

    modelo y controller validacion

--- app/Http/Controllers/ValoracionController.php
+++ app/Http/Controllers/ValoracionController.php
@@ -4,7 +4,59 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Valoracion; //llamar al modelo
+
 class ValoracionController extends Controller
 {
-    //
+    public function index(){
+        //SELECT * FROM perfil
+        $valoraciones= valoracion::all(); //Ejemplo del modelo
+        //Validando las validaciones 
+        if (count($valoraciones)<1)
+        {
+            return response()->jason(array(
+            'message'=> "No se encontraron valoraciones.",
+            'data'=>$valoraciones, //información de la tabla
+            'code'=>404,
+
+            ),404);
+        }
+        
+        return response()->json(array(
+            'message'=>"Valoraciones disponibles",
+            'data'=>$valoraciones,
+            'code'=>200,
+        ),200);
+    }
+
+   
+//3. Registro de nueva valoracion
+public function store(Request $request)
+{
+    $data =array(
+        'doctor_id',$request->doctor_id,
+        'citas_agendadas'=>$request->citas_agendadas,
+        'puntuacion'=>$request->puntuacion,
+        'comentario'=>$request->comentario,
+    
+    );
+    //INSERT INTO $valoracion () VALUES();
+    $newValoracion = new Paciente($data);
+
+    if ($newValoracion->save()== false){
+        return response()->json(array(
+            'message'=> "Valoración no procesada.",
+            'data'=> $newValoracion,
+            'code'=> 422,
+        ),422);
+    }
+    
+    return response()->json(array(
+        'message'=> "Valoración guardado con exito.",
+        'data'=> $newValoracion,
+        'code'=> 201, 
+    ),201);
+
+
+}
 }
--- app/Models/PacienteCita.php
+++ app/Models/PacienteCita.php
@@ -5,19 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Valoracion extends Model
+class PacienteCita extends Model
 {
     use HasFactory;
 
     //1. vincular la tabla al modelo
-    protected $table = "valoraciones";
+    protected $table = "PacienteCita";
     
     //2. Definicion de campos a modificar o insertar
-        protected $fillable = array(
-            'doctor_id',
-            'citas_agendadas',
-            'puntuacion',
-            'comentario', 
-            
-       );
+
+    protected $fillable = array(
+        'cita_id',
+        'paciente_id',
+        'doctor_id',
+    );
+   
 }
