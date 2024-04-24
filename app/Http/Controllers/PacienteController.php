b4ce721304f1a2528d49a13d953e6cbcd4e6678d
Shers <shersportillo@gmail.com>
2024-04-24 10:02:14 -0600

    Actualización de Pacientecontroller

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -17,17 +17,82 @@ class IngresoPacienteController extends Controller{
         if (count($pacientes)<1){
             return response()->json(array(
                 'message'=> "No se encontró perfiles de pacientes registrados.",
-                'data'=> $perfil_pacientes, //Extraer información
+                'data'=> $paciente, //Extraer información
                 'code'=> 404,
             ),404);
         }
         
         return response()->json(array(
             'message'=> "Perfiles de pacientes registrados",
-            'data'=> $perfil_pacientes,
+            'data'=> $paciente,
             'code'=> 200, 
         ),200);
-    
-
     }
+    public function show(Request $request, string $nnombre) //Es ua validacio "strig" sio se poe por defecto será ua cadea de texto
+    {
+        #Select * From Perfil paciete where nombre=? o ":nombre" LIMIT 
+        $paciente =Paciente::where('nombre','=',$nombre)->first();
+        #Validando que haya almennos un paciente
+        
+        if ($paciente==NULL){
+            return respose()->json(array(
+                'message'=>"Registro de Paciente no encotrado",
+                'data'=>$paciente,
+                'code'=>404,
+            ),404);
+        }
+        return response()->json(array(
+            'message'=>"Registro de paciente encontrado exitosamente",
+            'data'=>$paciente,
+            'code'=>200,
+        ),200);
+    }
+    # III. Registrar un nuevo Paciente
+    public function store(Request $request)
+    {
+        $data=array(
+            'citas_id',$request->citas_id,
+            'nombre_completo',$request->nombre_completo,
+            'email',$request->email,
+            'telefono',$request->telefono,
+            'password',$request->password,
+        );
+        #INSERT INTO $Paciente 
+        $newPaciente = new Paciente($data);
+
+        if ($newPaciente->save()==false){
+            return response()->json(array(
+                'message'=>"Registo de paciente no procesado",
+                'data'=>$newPaciente,
+                'code'=>422,
+            ),422);
+        }
+        return response()->json(array(
+            'message'=>"Paciente registrado con exito",
+            'data'=>$newPaciente,
+            'code'=>201,
+        ),201);
+    }
+
+        #Actualizar un paciente en especifico
+        public function update(Request $request, string $nombre)
+        {
+        # $request ->validated(); Archivo request
+
+        #1. Validar los datos
+        $paciente=Paciente::where('nombre','=',$nombre);
+
+        # 2. Verificar la existencia del registro
+        if ($Paciente== NULL){
+            return response()->json(array(
+                'mesage'=>"Registro de Paciente no encontrado",
+                'data'=>$paciente,
+                'code'=>404,
+            ),404);
+        }
+        
+        #3. Sobreescribimos la información existente
+        }
+
+
 }
